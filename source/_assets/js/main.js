73ebb074afceaf3a75d63f44d5cdf2e110d3ef17
mcddx330 <monobeat.030+github@gmail.com>
2025-01-13 00:09:00 +0900

    optimize fontawesome

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,3 +1,4 @@
-import "@fortawesome/fontawesome-free/js/all.min";
+import "@fortawesome/fontawesome-free/js/fontawesome.min";
+import "@fortawesome/fontawesome-free/js/solid.min";
 import "./scroll.js";
 import "./album.js";
